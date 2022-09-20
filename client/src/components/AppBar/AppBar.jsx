48282912202c00ac5307ab6d88c1978da14a11d3
Sokolov1993 <prokinoshow@gmail.com>
2022-09-20 17:38:47 +0300

    delete old app bar

--- client/src/components/AppBar/AppBar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import classes from './AppBar.module.scss';
-
-const AppBar = () => {
-  return <div></div>;
-};
-
-export default AppBar;
--- client/src/socketContext/socketContext.js
+++ client/src/socketContext/socketContext.js
@@ -5,7 +5,8 @@ import Peer from 'simple-peer';
 const SocketContext = createContext();
 
 // const socket = io('http://localhost:5000');
-const socket = io('https://web-chat-app-i-see-you.herokuapp.com/');
+// const socket = io('https://web-chat-app-i-see-you.herokuapp.com/');
+const socket = io('https://video-chat-serv.herokuapp.com/'); //new server
 
 const ContextProvider = ({ children }) => {
   const [stream, setStream] = useState(null);
